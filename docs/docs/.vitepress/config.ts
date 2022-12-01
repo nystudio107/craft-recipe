6cb383d47f7ef9b4e987aa0b30e6ce4e6940cdb0
Andrew Welch <andrew@nystudio107.com>
2022-12-01 16:53:19 -0500

    refactor: Switch to VitePress `^1.0.0-alpha.29` for the documentation

---
+++ docs/docs/.vitepress/config.ts
@@ -0,0 +1,38 @@
+import {defineConfig} from 'vitepress'
+
+export default defineConfig({
+  title: 'Recipe Plugin',
+  description: 'Documentation for the Recipe plugin',
+  base: '/docs/recipe/',
+  lang: 'en-US',
+  head: [
+    ['meta', {content: 'https://github.com/nystudio107', property: 'og:see_also',}],
+    ['meta', {content: 'https://twitter.com/nystudio107', property: 'og:see_also',}],
+    ['meta', {content: 'https://youtube.com/nystudio107', property: 'og:see_also',}],
+    ['meta', {content: 'https://www.facebook.com/newyorkstudio107', property: 'og:see_also',}],
+  ],
+  themeConfig: {
+    socialLinks: [
+      {icon: 'github', link: 'https://github.com/nystudio107'},
+      {icon: 'twitter', link: 'https://twitter.com/nystudio107'},
+    ],
+    logo: '/img/plugin-logo.svg',
+    editLink: {
+      pattern: 'https://github.com/nystudio107/craft-recipe/edit/develop/docs/docs/:path',
+      text: 'Edit this page on GitHub'
+    },
+    algolia: {
+      appId: 'ANVOBU7GYX',
+      apiKey: 'd9aa4fc31d67fba1d38915de67311070',
+      indexName: 'recipe'
+    },
+    lastUpdatedText: 'Last Updated',
+    sidebar: [],
+    nav: [
+      {text: 'Home', link: 'https://nystudio107.com/plugins/recipe'},
+      {text: 'Store', link: 'https://plugins.craftcms.com/recipe'},
+      {text: 'Changelog', link: 'https://nystudio107.com/plugins/recipe/changelog'},
+      {text: 'Issues', link: 'https://github.com/nystudio107/craft-recipe/issues'},
+    ]
+  },
+});
