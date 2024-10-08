63fc22cd380de281e66841d4f33e0f6455ba2a0c
Andrew Welch <andrew@nystudio107.com>
2024-10-08 00:25:44 -0400

    feat: Update for v5

--- docs/docs/.vitepress/config.ts
+++ docs/docs/.vitepress/config.ts
@@ -3,7 +3,7 @@ import {defineConfig} from 'vitepress';
 export default defineConfig({
   title: 'Recipe Plugin',
   description: 'Documentation for the Recipe plugin',
-  base: '/docs/recipe/v4/',
+  base: '/docs/recipe/',
   lang: 'en-US',
   head: [
     ['meta', {content: 'https://github.com/nystudio107', property: 'og:see_also',}],
@@ -18,7 +18,7 @@ export default defineConfig({
     ],
     logo: '/img/plugin-logo.svg',
     editLink: {
-      pattern: 'https://github.com/nystudio107/craft-recipe/edit/develop-v4/docs/docs/:path',
+      pattern: 'https://github.com/nystudio107/craft-recipe/edit/develop-v5/docs/docs/:path',
       text: 'Edit this page on GitHub'
     },
     algolia: {
@@ -26,7 +26,7 @@ export default defineConfig({
       apiKey: '66d1888afb505fa3d1b0342a487706ff',
       indexName: 'recipe',
       searchParameters: {
-        facetFilters: ["version:v4"],
+        facetFilters: ["version:v5"],
       },
     },
     lastUpdatedText: 'Last Updated',
@@ -37,9 +37,9 @@ export default defineConfig({
       {text: 'Changelog', link: 'https://nystudio107.com/plugins/recipe/changelog'},
       {text: 'Issues', link: 'https://github.com/nystudio107/craft-recipe/issues'},
       {
-        text: 'v4', items: [
-          {text: 'v5', link: 'https://nystudio107.com/docs/recipe/'},
-          {text: 'v4', link: '/'},
+        text: 'v5', items: [
+          {text: 'v5', link: '/'},
+          {text: 'v4', link: 'https://nystudio107.com/docs/recipe/v4/'},
           {text: 'v1', link: 'https://nystudio107.com/docs/recipe/v1/'},
         ],
       },
